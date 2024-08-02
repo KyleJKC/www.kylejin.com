7bb06c07fc7b2d95869587fbd68da589d59e6c1c
KyleJKC <kj@kylejin.com>
2024-08-02 12:54:11 +0800

    feat(hero): add typed cursor effect to hero component

---
+++ src/components/hero/TypedText.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect } from 'react';
+import Typed from 'typed.js';
+
+export function TypedText() {
+  useEffect(() => {
+    const poetry = [
+      '17歲，是學生',
+      '喜歡瞎折腾',
+      'Have some time?',
+      'Check out this place.',
+      '沒啥好說的',
+      '祝君好夢。'
+    ];
+    const typed = new Typed('.typed', {
+      strings: poetry,
+      typeSpeed: 40,
+      backSpeed: 40,
+      startDelay: 600,
+      
+    });
+    return () => typed.destroy();
+  }, []);
+
+  return (
+    <div className="">
+      <span className="typed leading-7 tracking-wide"></span>
+    </div>
+  );
+}
